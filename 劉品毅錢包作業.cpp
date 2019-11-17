537f9dd69c7235349952c1f618035ae6710a393e
liujimmy0124 <56680290+liujimmy0124@users.noreply.github.com>
2019-11-17 17:04:19 +0800

    建立 劉品毅錢包作業.cpp

---
+++ 劉品毅錢包作業.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "請輸入錢";
+
+    int coin;
+    cin >> coin;
+    for (c)
+    int x = coin / 100;
+
+    int y = coin % 100;        
+    
+    int z = y / 10;
+
+    int i = y % 10;
+    cout << x + z + i;
+}
+//655495 劉品毅
